fb1895cc7eda07d245cffffbefc886852572e05d
HungHcc <buisihung321@gmail.com>
2021-12-19 02:33:23 +0700

    feat: update privacy content

--- wp-content/themes/seymour-new/page-templates/page-privacy.php
+++ wp-content/themes/seymour-new/page-templates/page-privacy.php
@@ -57,46 +57,41 @@ $query_acco = new WP_Query($args);
         <section class="px-6">
             <div class="row justify-content-center pb-5">
                 <div class="col-12 col-md-8 col-lg-6">
-                    <p class="subtitle pb-3">Private Policy</p>
+                    <p class="subtitle pb-3">Personal Information</p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
-                        mauris quam pretium id rutrum scelerisque dictum velit dignissim.
-                        Libero molestie ut malesuada nulla malesuada tincidunt. Porttitor
-                        quis tellus nisi dui. Mauris elit netus semper mus elit odio
-                        metus, ut ipsum. Vestibulum enim sed porttitor pellentesque
-                        commodo augue. Tortor sed condimentum sollicitudin turpis
-                        imperdiet tortor mi vulputate magna. Cras aenean nunc orci, elit,
-                        ullamcorper.
+                        When you use the Seymour website, we collect personal information about you. We may not be able to supply you with all of the services you have requested if we do not have it. Contact Information, Account, Profile Information, Identity Verification, and Payment Information are all part of this information.
                     </p>
                 </div>
             </div>
             <div class="row justify-content-center pb-5">
                 <div class="col-12 col-md-8 col-lg-6">
-                    <p class="subtitle pb-3">Private Policy</p>
+                    <p class="subtitle pb-3">How we use information we collecy</p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
-                        mauris quam pretium id rutrum scelerisque dictum velit dignissim.
-                        Libero molestie ut malesuada nulla malesuada tincidunt. Porttitor
-                        quis tellus nisi dui. Mauris elit netus semper mus elit odio
-                        metus, ut ipsum. Vestibulum enim sed porttitor pellentesque
-                        commodo augue. Tortor sed condimentum sollicitudin turpis
-                        imperdiet tortor mi vulputate magna. Cras aenean nunc orci, elit,
-                        ullamcorper.
+                        Create and maintain a trusted and safer environment by detecting and preventing fraud, spam, abuse, security and safety incidents, and other harmful activity. Moreover, we provide payment services such as complying with legal obligations (such as anti-money laundering regulations), enforcing the Payment Terms and other payment policies.
                     </p>
                 </div>
             </div>
             <div class="row justify-content-center pb-5">
                 <div class="col-12 col-md-8 col-lg-6">
-                    <p class="subtitle pb-3">Private Policy</p>
+                    <p class="subtitle pb-3">Your Right</p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
-                        mauris quam pretium id rutrum scelerisque dictum velit dignissim.
-                        Libero molestie ut malesuada nulla malesuada tincidunt. Porttitor
-                        quis tellus nisi dui. Mauris elit netus semper mus elit odio
-                        metus, ut ipsum. Vestibulum enim sed porttitor pellentesque
-                        commodo augue. Tortor sed condimentum sollicitudin turpis
-                        imperdiet tortor mi vulputate magna. Cras aenean nunc orci, elit,
-                        ullamcorper.
+                        In some jurisdictions, applicable law may entitle you to request specific copies of your personal information or information about how we handle your personal information, as well as to request copies of personal information that you have provided to us in a structured, commonly used, and machine-readable format, and/or to request that we transmit this information to another service provider (where technically feasible).
+                    </p>
+                </div>
+            </div>
+            <div class="row justify-content-center pb-5">
+                <div class="col-12 col-md-8 col-lg-6">
+                    <p class="subtitle pb-3">Security</p>
+                    <p>
+                        While no organization can guarantee complete security, we are constantly developing and updating administrative, technical, and physical security measures to help safeguard your data from unauthorized access, loss, destruction, or alteration.
+                    </p>
+                </div>
+            </div>
+            <div class="row justify-content-center pb-5">
+                <div class="col-12 col-md-8 col-lg-6">
+                    <p class="subtitle pb-3">Credit Card or Bank Transfer</p>
+                    <p>
+                        Payment is safely processed from your credit/debit card or bank account to the bank account of the accommodation provider through a third-party payment processor. Check the (reservation) details of your choice thoroughly for any such conditions prior to making your reservation.
                     </p>
                 </div>
             </div>

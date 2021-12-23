4fb176c57f320798b3cb4fa8a5299e05d9e699e1
HungHcc <buisihung321@gmail.com>
2021-12-23 09:56:15 +0700

    fix: styling

--- wp-content/themes/seymour-new/inc/cta.php
+++ wp-content/themes/seymour-new/inc/cta.php
@@ -3,12 +3,12 @@
         <h1 class="cta__wrapper-title">Book your stay</h1>
         <p class="cta__wrapper-description">Find the best suit you</p>
         <div class="row justify-content-center mx-0">
-            <div class="col-8 col-md-3 col-xl-3 pb-2">
+            <div class="col-8 col-md-4 col-xl-3 pb-2">
                 <button data-toggle="modal" data-target="#book-modal-wrapper" class="btn w-100 btn-lg btn-primary" style="margin-right: 14px">
                     BOOK NOW
                 </button>
             </div>
-            <div class="col-8 col-md-3 col-xl-3">
+            <div class="col-8 col-md-4 col-xl-3">
                 <a href="/how-to-book">
                     <button class="btn w-100 btn-lg btn-outline-primary">
                         HOW TO BOOK
